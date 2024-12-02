a59988d99d9c90a26630515c660abcacf48f0014
“kasa4007” <kasa4007ksato1@gmail.com>
2024-12-02 17:01:15 +0900

    課題Aが終了

---
+++ a-5.py
@@ -0,0 +1,6 @@
+bob_info = ["Bob", "Dylan", 79]
+
+print(f'"Name: {bob_info[0]} {bob_info[1]}, Age: {bob_info[2]}"')
+
+# 出力結果
+# "Name: Bob Dylan, Age: 79"
---
+++ a-7.py
@@ -0,0 +1,7 @@
+even_numbers = [2, 4, 6, 8]
+
+for number in even_numbers:
+    print(number * 2)
+
+# for を使って even_numbers のそれぞれの値を2倍した値を出力してください
+# 4, 8, 12, 16
