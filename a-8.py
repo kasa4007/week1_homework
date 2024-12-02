98da11b4b09a086077953a8b08f0069e2960a15c
“kasa4007” <kasa4007ksato1@gmail.com>
2024-12-02 20:55:09 +0900

    1週目の課題Aを修正

--- a-8.py
+++ a-8.py
@@ -1,10 +1,11 @@
 users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
 
-print(f"Name: {users_info[0][0]}, Age: {users_info[0][1]}")
-print(f"Name: {users_info[1][0]}, Age: {users_info[1][1]}")
-print(f"Name: {users_info[2][0]}, Age: {users_info[2][1]}")
+for user in users_info:
+    print(f"Name: {user[0]}, Age: {user[1]}")
 
-# for user in users_info:
+# print(f"Name: {users_info[0][0]}, Age: {users_info[0][1]}")
+# print(f"Name: {users_info[1][0]}, Age: {users_info[1][1]}")
+# print(f"Name: {users_info[2][0]}, Age: {users_info[2][1]}")
 
 # 出力結果
 # Name: Bob, Age: 79
